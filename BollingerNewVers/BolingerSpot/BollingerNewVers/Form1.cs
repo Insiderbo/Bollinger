6fe501e3f992712f17ff9f81f107a91f4c7251ee
lan\insiderbo <91372@mail.ru>
2022-03-11 18:15:45 +0700

    последние изменения

---
+++ BollingerNewVers/BolingerSpot/BollingerNewVers/Form1.cs
@@ -0,0 +1,199 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BollingerNewVers
+{
+
+    public partial class Form1 : Form
+    {
+        private string namePara;
+        private int period;
+
+        Dictionary<string, List<string>> allOrders = new Dictionary<string, List<string>>();
+
+        public Form1()
+        {
+            AddAllOrders();
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CycleWork();
+            comboBox1.Enabled = false;
+            comboBox2.Enabled = false;
+            //comboBox3.Enabled = false;
+            //comboBox4.Enabled = false;
+        }
+        private async void CycleWork()
+        {
+            while (true)
+            {
+                if (comboBox1.Text == "")
+                {
+                    MessageBox.Show("No order");
+                    return;
+                }
+                if (comboBox2.Text == "")
+                {
+                    MessageBox.Show("No period");
+                    return;
+                }
+                 BollingerSpotMarket.Telegram.prozents = new Dictionary<string, string>() {
+                        {"comboBox3", comboBox3.Text},
+                        {"comboBox4", comboBox4.Text},
+                        {"comboBox5", comboBox5.Text}
+                    };
+                 BollingerSpotMarket.Telegram.checkBoxs = new Dictionary<string, bool>()
+                    {
+                        {"checkBox1", checkBox1.Checked},
+                        {"checkBox2", checkBox2.Checked}
+                    };
+                namePara = comboBox1.Text;
+                period = Convert.ToInt32(comboBox2.Text);
+                button1.Enabled = false;
+                await StartWork();
+                await Task.Delay(period * 1000);
+            }
+        }
+        public async Task<string> LoadUrlAsText(string url)
+        {
+            var request = WebRequest.Create(url);
+            using (var response = await request.GetResponseAsync())
+            {
+                using (var stream = response.GetResponseStream())
+                {
+                    var sr = new StreamReader(stream);
+                    return await sr.ReadToEndAsync();
+                }
+            }
+        }
+        public async Task StartWork()
+        {
+            foreach (var item in allOrders[namePara])
+            {
+                Bollenger(item);
+            }
+        }
+        public async Task Bollenger(string para)
+        {
+            label3.Text = allOrders[namePara].Count.ToString();
+
+            string intervals = comboBox5.Text.ToString();
+            dynamic d = await LoadUrlAsText($"https://api.binance.com/api/v1/klines?symbol={para}&interval={intervals}&limit=21");
+            dynamic allOrder = JsonConvert.DeserializeObject(d);
+
+            double totalAverage = 0;
+            double totalSquares = 0;
+            double lastprice = 0;
+            double openPrice = 0;
+            double closePrice = 0;
+            double сlosedOpen = Convert.ToDouble(allOrder[19][1]);//[JSON].[19].[1] Open
+            double сlosedClouse = Convert.ToDouble(allOrder[19][4]);//[JSON].[19].[4] Clouse
+            try
+            {
+                dynamic www = await LoadUrlAsText($"https://api.binance.com/api/v3/ticker/price?symbol={para}");
+                dynamic lastPare = JsonConvert.DeserializeObject(www);
+                lastprice = (Convert.ToDouble(lastPare.price));
+            }
+            catch { }
+
+            //[JSON].[0].[4]
+            foreach (dynamic item in allOrder)
+            {
+                openPrice = (Convert.ToDouble(item[1]));//[JSON].[0].[1]
+                closePrice = (Convert.ToDouble(item[4]));
+                totalAverage += closePrice;//итоговая цена
+                totalSquares += Math.Pow(Math.Round(closePrice, 8), 2);//возводим в квадрат средние цены закрытия
+            }
+
+            double average = totalAverage / allOrder.Count;
+            double stdev = Math.Sqrt((totalSquares - Math.Pow(totalAverage, 2) / allOrder.Count) / allOrder.Count);
+            double up = average + 2 * stdev;
+            double down = average - 2 * stdev;
+            double bandWidth = (up - down) / average;
+            double procup = 1 + double.Parse(comboBox4.Text) / 100;
+            double upproc = Math.Round((up * procup), 8);
+            double procdown = 1 + double.Parse(comboBox3.Text) / 100;
+            double downproc = Math.Round((down / procdown), 8);
+
+            label1.Text = "Pair " + para;
+
+            if (upproc != double.NaN && downproc != double.NaN)
+            {
+                await BollingerSpotMarket.Telegram.IndexForTelegramm(para,
+                     new Dictionary<string, double>()
+                     {
+                    {"average", average },
+                    {"stdev", stdev },
+                    {"up", up },
+                    {"down", down},
+                    {"bandWidth", bandWidth },
+                    {"procup", procup},
+                    {"upproc", upproc },
+                    {"procdown", procdown },
+                    {"downproc", downproc },
+                    {"lastprice", lastprice },
+                    {"openPrice", openPrice },
+                    {"closePrice", closePrice },
+                    {"сlosedOpen",сlosedOpen },
+                    {"сlosedClouse", сlosedClouse }
+                     });
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            BollingerSpotMarket.Telegram.ClearCheckedOrders();
+            button1.Enabled = true;
+            comboBox1.Enabled = true;
+            comboBox2.Enabled = true;
+            comboBox3.Enabled = true;
+            comboBox4.Enabled = true;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            BollingerSpotMarket.Telegram.AddOrderForMonitoring(textBox1.Text.ToString());
+            textBox1.Text = "";
+        }
+        async void  AddAllOrders()
+        {
+            dynamic allPares = JsonConvert.DeserializeObject(await LoadUrlAsText("https://api.binance.com/api/v3/exchangeInfo"));
+            
+            foreach (var item in allPares.symbols)
+            {
+                if (allOrders.ContainsKey(item.quoteAsset.ToString()))
+                {                    
+                    allOrders[item.quoteAsset.ToString()].Add(item.symbol.ToString());
+                }
+                else
+                {
+                    List<string> orders = new List<string>();
+                    orders.Add(item.symbol.ToString());
+                    allOrders.Add(item.quoteAsset.ToString(), orders);
+                }                
+            }
+        }
+
+        private async void button4_Click(object sender, EventArgs e)
+        {
+           var arg = "Test bot";
+           await BollingerSpotMarket.Telegram.TelegramBotRepuschae(arg);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            BollingerSpotMarket.Telegram.ClearMonitoring();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ BollingerNewVers/BolingerSpot/BollingerNewVers/TelegramBot.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Binance.Net.Clients;
+using CryptoExchange.Net.Authentication;
+using Binance.Net.Objects;
+using CryptoExchange.Net.Objects;
+using Binance.Net.Enums;
+
+namespace BollingerSpotMarket
+{
+    static class Telegram
+    {
+        static List<string> resalt = new List<string>();
+        static List<string> monitoring = new List<string>();
+        static List<string> downmonitor = new List<string>();
+        static List<string> upmonitor = new List<string>();
+        public static Dictionary<string, string> prozents;
+        public static Dictionary<string, bool> checkBoxs;
+
+        static ITelegramBotClient botClient;
+
+        static public void ClearMonitoring()
+        {
+            monitoring.Clear();
+        }
+
+        static public void ClearCheckedOrders()
+        {
+            resalt.Clear();
+            monitoring.Clear();
+            downmonitor.Clear();
+            downmonitor.Clear();
+            upmonitor.Clear();
+        }
+        static public void AddOrderForMonitoring(string order)
+        {
+            monitoring.Add(order);
+        }
+        async static public Task IndexForTelegramm(string para, Dictionary<string, double> indicators)
+        {
+            if (resalt.Contains(para) == false)
+            {
+                if (indicators["lastprice"] < indicators["downproc"] && checkBoxs["checkBox2"] == true)
+                {
+                    var args = "Возможенo Long. Точность 70%. Пробой ==-> " + prozents["comboBox3"] + " % " + "\n" + para.ToString() + "\n" + "Цена ==-> " + Math.Round(indicators["lastprice"], 8).ToString() + "\n" + "Таймфрейм ==-> " + prozents["comboBox5"];
+                    TelegramBot(args);
+                    resalt.Add(para);
+                    downmonitor.Add(para);
+                }
+                if (indicators["lastprice"] > indicators["upproc"] && checkBoxs["checkBox1"] == true)
+                {
+                    var args = "Возможно Short. Точность 70%. Пробой ==->  " + prozents["comboBox4"] + " % " + "\n" + para.ToString() + "\n" + "Цена ==-> " + Math.Round(indicators["lastprice"], 8).ToString() + "\n" + "Таймфрейм ==-> " + prozents["comboBox5"];
+                    TelegramBot(args);
+                    resalt.Add(para);
+                    upmonitor.Add(para);
+                }
+            }
+            if (indicators["lastprice"] < indicators["average"] && upmonitor.Contains(para) == true)
+            {
+                resalt.Remove(para);
+            }
+            if (indicators["lastprice"] > indicators["average"] && downmonitor.Contains(para) == true)
+            {
+                resalt.Remove(para);
+            }
+
+            if (downmonitor.Contains(para) == true)
+            {
+                if (indicators["lastprice"] > indicators["down"] && indicators["сlosedClouse"] > indicators["сlosedOpen"])
+                {
+                    var args = "Возможенo Long. Точность 90%. ==->  " + para.ToString() + "\n" + "Цена ==-> " + Math.Round(indicators["lastprice"], 8).ToString() + "\n" + "Таймфрейм ==-> " + prozents["comboBox5"];
+                    TelegramBot(args);
+                    downmonitor.Remove(para);
+                }
+            }
+            if (upmonitor.Contains(para) == true)
+            {
+                if (indicators["lastprice"] < indicators["up"] && indicators["сlosedClouse"] < indicators["сlosedOpen"])
+                {
+                    var args = "Возможно Short. Точность 90% ==->  " + para.ToString() + "\n" + "Цена ==-> " + Math.Round(indicators["lastprice"], 8).ToString() + "\n" + "Таймфрейм ==-> " + prozents["comboBox5"];
+                    TelegramBot(args);
+                    upmonitor.Remove(para);
+                }
+            }
+
+            if (monitoring.Contains(para) == true)
+            {
+                if (indicators["lastprice"] < indicators["downproc"])
+                {
+                    var arg = "Монета на контроле " + "\n" + "Возможно Long ==-> " + prozents["comboBox3"] + " % " + "\n" + para.ToString() + "\n" + "Цена ==-> " + Math.Round(indicators["lastprice"], 8).ToString() + "\n" + "Таймфрейм ==-> " + prozents["comboBox5"];
+                    TelegramBotRepuschae(arg);
+                }
+                if (indicators["lastprice"] > indicators["upproc"])
+                {
+                    var arg = "Монета на контроле " + "\n" + "Возможно Short ==->  " + prozents["comboBox4"] + " % " + "\n" + para.ToString() + "\n" + "Цена ==-> " + Math.Round(indicators["lastprice"], 8).ToString() + "\n" + "Таймфрейм ==-> " + prozents["comboBox5"];
+                    TelegramBotRepuschae(arg);
+
+                }
+            }
+        }
+        static async Task TelegramBot(string args)
+        {
+            botClient = new TelegramBotClient("5167308233:AAGF2mu55byq8XKBXxo7SKFOke7rB1tc5_8");
+            var chat_id = -1001741001182;
+            await SendMessageAsync(chat_id, args);
+        }
+        static async Task SendMessageAsync(long chatId, string args)
+        {
+            await botClient.SendTextMessageAsync(chatId: chatId, text: args);
+        }
+        public static async Task TelegramBotRepuschae(string arg)
+        {
+            botClient = new TelegramBotClient("5059700101:AAGpy77Pjg_vmX4aVSXYyS4oa00U_cyEMOA");
+            var chat_id = -1001714789241;
+            await SendMessageAsyncRepurchase(chat_id, arg);
+        }
+        static async Task SendMessageAsyncRepurchase(long chatId, string arg)
+        {
+            await botClient.SendTextMessageAsync(chatId: chatId, text: arg);
+        }
+
+
+    }
+}
